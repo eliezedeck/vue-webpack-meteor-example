a7c8ce1464390ce807eaebce8c5eae3f73239163
Elie Zedeck <elie.zedeck@gmail.com>
2016-09-09 21:33:31 +0300

    Properly load everything.

--- .client/main.js
+++ .client/main.js
@@ -1,6 +1,10 @@
-var Vue = require('vue')
-var App = Vue.extend(require('./app/app.vue'))
+var Vue = require('vue');
+var App = Vue.extend(require('./app/app.vue'));
 
-Template.body.onRendered(function () {
-  new App({ el: '#app' })
-})
+// Template.body.onRendered(function () {
+//   new App({ el: '#app' })
+// });
+
+// Normally, this should be wrapped above, but the loading of the Webpack file is so late, that the Template.body is
+// already loaded and its onRendered method executed. So we can immediately just launch the App.
+new App({ el: '#app' });
